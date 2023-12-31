54c1c279504406b6b81c641ebdea2d48038d9881
Ruma303 <matteoh8@hotmail.it>
2023-12-31 10:48:02 +0100

    commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//% Axios
+import { HTTP } from "./Utils/Axios.js";
+const App = Vue.createApp({
+    data() {
+        return {
+            posts: [],
+            error: null,
+            isLoading: false,
+        };
+    }, methods: {
+        async fetchPosts() {
+            this.isLoading = true;
+            try {
+                const posts = await HTTP.get('/posts');
+                this.posts = posts.data;
+            } catch (error) {
+                this.error = error;
+            } finally {
+                this.isLoading = false;
+            }
+        }
+    },
+    mounted() {
+        this.fetchPosts();
+    }
+});
+const mountedApp = App.mount('#app');
+
+
+//% fetch
+/* const App = Vue.createApp({
+    data() {
+        return {
+            posts: [],
+            error: null,
+            isLoading: false,
+        };
+    },
+    methods: {
+        fetchPosts() {
+            this.isLoading = true;
+            fetch('https://jsonplaceholder.typicode.com/posts')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    this.posts = data;
+                })
+                .catch(error => {
+                    this.error = error;
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        }
+    },
+    mounted() {
+        this.fetchPosts();
+    },
+});
+const mountedApp = App.mount('#app'); */
+
